831da423e56707e7cb2fb5ce4a155693aedd161f
Thiago <tms1_tms2@hotmail.com>
2021-10-06 15:02:40 -0300

    SignIn Component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,37 @@
-export default function Home() {
+import { Flex, Button, Stack } from '@chakra-ui/react';
+import { Input } from '../components/Form/Input';
+
+export default function SignIn() {
   return (
-    <h1>Dashgo</h1>
+    <Flex
+      w="100vw"
+      h="100vh"
+      align="center"
+      justify="center"
+    >
+      <Flex
+        as="form"
+        w="100%"
+        maxW={360}
+        bg="gray.800"
+        p="2rem"
+        borderRadius={8}
+        flexDir="column"
+      >
+        <Stack spacing="1rem">
+          <Input type="email" label="Email" name="email"/>
+          <Input type="password" label="Senha" name="password"/>
+        </Stack>
+
+        <Button
+          type="submit"
+          mt="1.5rem"
+          colorScheme="pink"
+          size="lg"
+        >
+          Entrar
+        </Button>
+      </Flex>
+    </Flex>
   )
 }
