6725ba6733023d1d7806cbe1c988f913737c511f
Thiago <tms1_tms2@hotmail.com>
2021-10-06 23:36:53 -0300

    Sidebar component

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,7 +1,15 @@
+import { Flex } from "@chakra-ui/react";
 import { Header } from "../components/Header";
+import { Sidebar } from "../components/Sidebar";
 
 export default function Dashboard() {
   return (
-    <Header />
+    <Flex direction="column" h="100vh">
+      <Header />
+
+      <Flex w="100%" my="1.5rem" mx="auto" maxW={1480} px="1.5rem" >
+        <Sidebar />
+      </Flex>
+    </Flex>
   );
 }
