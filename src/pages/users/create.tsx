e5c42da06b2df1d54abaf76f6f4ad66b17ef75cd
Thiago <tms1_tms2@hotmail.com>
2021-10-11 10:59:50 -0300

    Create user form

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -9,11 +9,43 @@ import {
   SimpleGrid,
   VStack,
 } from "@chakra-ui/react";
+import { SubmitHandler, useForm } from 'react-hook-form';
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
+
 import { Input } from "../../components/Form/Input";
 import { Header } from "../../components/Header";
 import { Sidebar } from "../../components/Sidebar";
 
+type CreateUserFormData = {
+  name: string;
+  email: string;
+  password: string;
+  password_confirmation: string;
+}
+
+const createUserFormSchema = yup.object().shape({
+  name: yup.string().required("Nome obrigatório"),
+  email: yup.string().required("Email obrigatório").email("Email inválido"),
+  password: yup.string().required("Senha obrigatória").min(6, "No mínimo 6 caracteres"),
+  password_confirmation: yup.string().oneOf([
+    null, yup.ref("password"),
+  ], "As senhas precisam ser iguais"),
+});
+
 export default function CreateUser() {
+  const { register, handleSubmit, formState } = useForm({
+    resolver: yupResolver(createUserFormSchema),
+  });
+
+  const { errors } = formState;
+
+  const handleCreateUser: SubmitHandler<CreateUserFormData> = async (values) => {
+    await new Promise(resolve => setTimeout(resolve, 2000));
+
+    console.log(values);
+  }
+
   return (
     <Box>
       <Header />
@@ -21,22 +53,48 @@ export default function CreateUser() {
       <Flex w="100%" my="1.5rem" mx="auto" maxW={1480} px="1.5rem" >
         <Sidebar />
 
-        <Box flex="1" borderRadius={8} bg="gray.800" p={["1.5rem", "2rem"]}>
+        <Box
+          as="form"
+          flex="1"
+          borderRadius={8}
+          bg="gray.800"
+          p={["1.5rem", "2rem"]}
+          onSubmit={handleSubmit(handleCreateUser)}
+        >
           <Heading size="lg" fontWeight="normal">Criar Usuário</Heading>
 
           <Divider my="1.5rem" borderColor="gray.700" />
 
           <VStack spacing="2rem">
             <SimpleGrid minChildWidth="240px" spacing={["1.5rem", "2rem"]} w="100%">
-              <Input name="name" label="Nome completo" />
-              <Input name="email" label="Email" type="email" />
+              <Input
+                name="name"
+                label="Nome completo"
+                error={errors.name}
+                {...register("name")}
+              />
+              <Input
+                name="email"
+                label="Email"
+                type="email"
+                error={errors.email}
+                {...register("email")}
+              />
             </SimpleGrid>
             <SimpleGrid minChildWidth="240px" spacing={["1.5rem", "2rem"]} w="100%">
-              <Input name="password" label="Senha" type="password" />
+              <Input
+                name="password"
+                label="Senha"
+                type="password"
+                error={errors.password}
+                {...register("password")}
+              />
               <Input
                 name="password_confirmation"
                 label="Confirmação de senha"
                 type="password"
+                error={errors.password_confirmation}
+                {...register("password_confirmation")}
               />
             </SimpleGrid>
           </VStack>
@@ -46,7 +104,13 @@ export default function CreateUser() {
               <Link href="/users" passHref>
                 <Button as="a" colorScheme="whiteAlpha">Cancelar</Button>
               </Link>
-              <Button colorScheme="pink">Salvar</Button>
+              <Button
+                type="submit"
+                colorScheme="pink"
+                isLoading={formState.isSubmitting}
+              >
+                Salvar
+              </Button>
             </HStack>
           </Flex>
         </Box>
--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -16,6 +16,7 @@ import {
   useBreakpointValue,
 } from "@chakra-ui/react";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
+
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
